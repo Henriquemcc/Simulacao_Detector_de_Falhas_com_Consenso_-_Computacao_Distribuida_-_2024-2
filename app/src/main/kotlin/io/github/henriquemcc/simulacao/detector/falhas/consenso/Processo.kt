7a25e76a71f3c97bef0358ef53e0929a7bd097a9
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-11-08 20:04:22 -0300

    Documentando a classe Processo

--- app/src/main/kotlin/io/github/henriquemcc/simulacao/detector/falhas/consenso/Processo.kt
+++ app/src/main/kotlin/io/github/henriquemcc/simulacao/detector/falhas/consenso/Processo.kt
@@ -3,10 +3,26 @@ package io.github.henriquemcc.simulacao.detector.falhas.consenso
 import java.util.concurrent.atomic.AtomicBoolean
 import kotlin.random.Random
 
+/**
+ * Classe que simula o funcionamento de um processo.
+ * @param id ID do processo.
+ * @param detectorFalhasConsenso Classe que instanciou esta classe.
+ */
 class Processo(private val id: Int, private val detectorFalhasConsenso: DetectorFalhasConsenso): Thread() {
 
+    /**
+     * Indica se o processo atual está falho.
+     */
     private var falho = AtomicBoolean(false)
 
+    /**
+     * Conjunto com os ids dos processos falhos.
+     */
+    private val processosFalhos = sortedSetOf<Int>()
+
+    /**
+     * Executa a simulação do funcionamento de um processo.
+     */
     override fun run() {
         println("Processo $id está em execução")
         while (!detectorFalhasConsenso.stopFlag.get()) {
