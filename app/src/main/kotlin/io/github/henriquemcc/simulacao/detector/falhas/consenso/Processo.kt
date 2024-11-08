46355fc9e269c7a394ce2a51cb0725d5e118d467
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-11-08 19:52:28 -0300

    Desenvolvendo a simulação da falha

--- app/src/main/kotlin/io/github/henriquemcc/simulacao/detector/falhas/consenso/Processo.kt
+++ app/src/main/kotlin/io/github/henriquemcc/simulacao/detector/falhas/consenso/Processo.kt
@@ -1,9 +1,29 @@
 package io.github.henriquemcc.simulacao.detector.falhas.consenso
 
-import java.util.concurrent.atomic.AtomicLong
+import java.util.concurrent.atomic.AtomicBoolean
+import kotlin.random.Random
 
 class Processo(private val id: Int, private val detectorFalhasConsenso: DetectorFalhasConsenso): Thread() {
+
+    private var falho = AtomicBoolean(false)
+
     override fun run() {
         println("Processo $id está em execução")
+        while (!detectorFalhasConsenso.stopFlag.get()) {
+            sleep(Random.nextLong(15000))
+
+            // Falhando
+            falho.set(Random.nextBoolean())
+            while (falho.get()) {
+                println("O processo $id está falho")
+                sleep(Random.nextLong(60000))
+
+                // Saindo da falha
+                falho.set(Random.nextBoolean())
+
+                if (!falho.get())
+                    println("O processo $id se recuperou da falha")
+            }
+        }
     }
 }
